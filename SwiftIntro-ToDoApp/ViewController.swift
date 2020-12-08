1a1d3e641b6ba285ba792aab8ea82692f8ed0763
Megan Herold <megan.mcpherson@alaskaair.com>
2020-12-07 19:00:11 -0800

    MH: Initial Project Setup

---
+++ SwiftIntro-ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftIntro-ToDoApp
+//
+//  Created by Megan Herold on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
