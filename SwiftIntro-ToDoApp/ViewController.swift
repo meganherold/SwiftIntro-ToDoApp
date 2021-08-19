f0a24afb5006691d8e311bffcd4991661d3a985e
Megan Herold <megan.mcpherson@alaskaair.com>
2021-08-19 09:37:05 -0700

    MH: progress after day 2 tableView demo

--- SwiftIntro-ToDoApp/ViewController.swift
+++ SwiftIntro-ToDoApp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        UITableView(frame: CGRect(x: 0,
+                                  y: 0,
+                                  width: UIScreen.main.bounds.width,
+                                  height: UIScreen.main.bounds.height))
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        setup()
+        setupSubviews()
     }
-
-
+    
+    func setup() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
+    
+    func setupSubviews() {
+        view.addSubview(tableView)
+    }
 }
 
